a7d2840c3525e0344b6ce86cf0f7365338f2b676
fsword <li.jianye@gmail.com>
2014-07-07 10:56:05 +0800

    change target of testcase

--- spec/integration/simple_story_spec.rb
+++ spec/integration/simple_story_spec.rb
@@ -3,7 +3,7 @@ module Larrow
   module Runner 
     describe "integration" do
       [
-        'git@github.com:fsword/larrow-qingcloud.git'
+        'git@github.com:fsword/essh.git'
       ].each do |url|
         it 'simple story' do
           expect(Manager.new(url).go).not_to be_nil
