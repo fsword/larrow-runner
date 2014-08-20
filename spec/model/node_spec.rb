4c0285d99c59a2608a9d0d43af932cec6ab57953
fsword <li.jianye@gmail.com>
2014-08-20 22:42:51 +0800

    rspec passed

--- spec/model/node_spec.rb
+++ spec/model/node_spec.rb
@@ -4,6 +4,9 @@ require 'ostruct'
 module Larrow::Runner::Model
   describe Node do
     subject{ Node.new nil, OpenStruct.new(address: 'localhost'), `whoami`.chomp }
+    before do 
+      Larrow::Runner::Option[:debug] = nil
+    end
     it 'can execute' do
       outputs = ""
       script = OpenStruct.new(actual_command: 'pwd')
