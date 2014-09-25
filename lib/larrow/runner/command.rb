004ad9af6d5b63226f87c4eab670107acd839636
fsword <li.jianye@gmail.com>
2014-09-26 00:32:24 +0800

    update command description

--- lib/larrow/runner/command.rb
+++ lib/larrow/runner/command.rb
@@ -25,7 +25,7 @@ larrow will build a whole world for your application
       desc 'dump [URL]','convert and dump configuration of the project'
       long_desc <<-EOF
 Read other CI file(eg:.travis.yml), conert to larrow style and dump to STDOUT. 
-You can save it as .larrow.yml on the project folder.
+You can save it as .larrow.yml on the project root folder.
       EOF
       def dump url
         vcs = Vcs.detect url
